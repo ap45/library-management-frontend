cbcfc26ef3fc2f60db94aaf64b379018604c262e
ap45 <arpit.parekh0104@gmail.com>
2024-11-03 14:29:58 -0500

    added pipeline code

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -2,69 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import "./Checkout.css";
 
-// const Checkout = () => {
-//   const [customerId, setCustomerId] = useState("");
-//   const [itemId, setItemId] = useState("");
-//   const [message, setMessage] = useState("");
-//   const navigate = useNavigate();
 
-//   // Change anything starting at 21 for backend
-//   const handleCheckout = async () => {
-//     try {
-//       const response = await fetch(`/api/check_out/${customerId}/${itemId}/`, { //Change this im not
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       });
-
-//       if (response.ok) {
-//         const data = await response.json();
-//         setMessage(data.message);
-//       } else {
-//         const error = await response.json();
-//         setMessage(error.message);
-//       }
-//     // eslint-disable-next-line no-unused-vars
-//     } catch (error) {
-//       setMessage("Error checking out the item.");
-//     }
-//   };
-
-//   // Navigate to the ManageFines page
-  // const handlePayFines = () => {
-  //   navigate("./ManageFines.jsx"); 
-  // };
-
-//   return (
-//     <div>
-//       <h2>Checkout</h2>
-//       <div>
-//         <label>Customer ID:</label>
-//         <input
-//           type="text"
-//           value={customerId}
-//           onChange={(e) => setCustomerId(e.target.value)}
-//         />
-//       </div>
-//       <div>
-//         <label>Item ID:</label>
-//         <input
-//           type="text"
-//           value={itemId}
-//           onChange={(e) => setItemId(e.target.value)}
-//         />
-//       </div>
-//       <button onClick={handleCheckout}>Checkout</button>
-//       <button onClick={handlePayFines}>Manage Fines</button> 
-//       {message && <p>{message}</p>}
-//     </div>
-//   );
-// };
-
-// export default Checkout;
-
-// Update Checkout.js
 
 const Checkout = () => {
   const [customerId, setCustomerId] = useState("");
@@ -78,12 +16,14 @@ const Checkout = () => {
       setMessage("Please enter both Customer ID and Item IDs");
       return;
     }
+    const API_URL = import.meta.env.VITE_BACKEND_URL;
+
 
     const itemIdArray = itemIds.split(',').map(id => id.trim()).filter(id => id); // Split and trim item IDs
     setLoading(true);
     try {
       const response = await fetch(
-        `http://localhost:8000/api/check_out/${customerId}/`, // Update the URL to use only customer ID
+        `${API_URL}/api/check_out/${customerId}/`, // Update the URL to use only customer ID
         {
           method: "POST",
           headers: {
