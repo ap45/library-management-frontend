87f20af3ab7aa2d11908b9b46043a24dd114003c
CaptainCaptain05 <aobrienapex@gmail.com>
2024-11-03 12:09:54 -0500

    fixed rendering issue
    Updated the structure of App.jsx so that the content area updates correctly to display the desired component when the route changes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../navigation/Navigation";
-import Checkout from "../checkout/Checkout";
 
 const Layout = () => {
 
@@ -20,15 +19,6 @@ const Layout = () => {
         flex: "10.2",
         minHeight: "100vh"
     }
-    const checkoutDivStyle = { // this will be removed after we figure out why the routing is not working
-        width: "100px", 
-        minHeight: "100vh",
-        padding: "10px", 
-        backgroundColor: "#f9f9f9", 
-        boxShadow: "0 0 0 2px rgba(160, 160, 160, 0.5)", 
-        flex:"500",
-        
-    }
 
     return (    
         <div style={pageDivStyle}>
@@ -37,9 +27,6 @@ const Layout = () => {
             </div>
             <div style={contentDivStyle}>
                 <Outlet />
-            </div>
-            <div style = {checkoutDivStyle}>  {/* Remoove this div after we figure out the routing problem */}
-                <Checkout />
             </div>
         </div>
     )
